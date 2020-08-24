9cbff5fd0465de680b10044fe388b7927ddcaa95
Sravan <65145081+ZephyrLabs@users.noreply.github.com>
2020-08-24 19:28:08 +0530

    Update Clock.cpp

--- src/DisplayApp/Screens/Clock.cpp
+++ src/DisplayApp/Screens/Clock.cpp
@@ -7,6 +7,7 @@
 #include "BatteryIcon.h"
 #include "BleIcon.h"
 #include "Symbols.h"
+#include "Gauge.h"
 using namespace Pinetime::Applications::Screens;
 extern lv_font_t jetbrains_mono_extrabold_compressed;
 extern lv_font_t jetbrains_mono_bold_20;
@@ -40,7 +41,37 @@ Clock::Clock(DisplayApp* app,
   lv_label_set_text(bleIcon, Symbols::bluetooth);
   lv_obj_align(bleIcon, batteryPlug, LV_ALIGN_OUT_LEFT_MID, -5, 0);
 
+//analog
+ 
+  /*Create a style*/
+  lv_style_copy(&style, &lv_style_pretty_color);
+  style.body.main_color = LV_COLOR_WHITE;     /*Line color at the beginning*/
+  style.body.grad_color =  LV_COLOR_WHITE;    /*Line color at the end*/
+  style.body.padding.left = 10;                      /*Scale line length*/
+  style.body.padding.inner = 8 ;                    /*Scale label padding*/
+  style.body.border.color = lv_color_hex3(0x333);   /*Needle middle circle color*/
+  style.line.width = 3;
+  style.text.color = LV_COLOR_WHITE;
+  style.line.color = LV_COLOR_WHITE;                  /*Line color after the critical value*/
 
+  /*Describe the color for the needles*/
+
+  needle_colors[0] = LV_COLOR_ORANGE;
+
+  /*Create a gauge*/
+  gauge1 = lv_gauge_create(lv_scr_act(), NULL);
+  lv_gauge_set_style(gauge1, LV_GAUGE_STYLE_MAIN, &style);
+  lv_gauge_set_needle_count(gauge1, 1, needle_colors);
+  lv_obj_set_size(gauge1, 180, 180);
+  lv_obj_align(gauge1, NULL, LV_ALIGN_CENTER, 0, 0);
+  lv_gauge_set_scale(gauge1, 360, 60, 0);
+  lv_gauge_set_range(gauge1, 0, 59);
+
+  /*Set the values*/
+  lv_gauge_set_value(gauge1, 0, value);                                            
+                                             
+//analog  
+                                             
   label_date = lv_label_create(lv_scr_act(), NULL);
   lv_obj_align(label_date, lv_scr_act(), LV_ALIGN_IN_LEFT_MID, 40, 80);
 
