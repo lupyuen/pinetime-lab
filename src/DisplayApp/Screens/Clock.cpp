0b0c8a885b2abe40ffc782e626365383fc6e8ad9
Sravan <65145081+ZephyrLabs@users.noreply.github.com>
2020-09-07 08:45:43 +0530

    Update Clock.cpp

--- src/DisplayApp/Screens/Clock.cpp
+++ src/DisplayApp/Screens/Clock.cpp
@@ -37,7 +37,7 @@ static void event_handler(lv_obj_t * obj, lv_event_t event) {
   needle_colors[0] = LV_COLOR_ORANGE;
 
   /*Create a gauge*/
-  gauge1 = lv_gauge_create(lv_scr_act(), NULL);
+  lv_obj_t * gauge1 = lv_gauge_create(lv_scr_act(), NULL);
 //lv_gauge_set_style(gauge1, LV_GAUGE_STYLE_MAIN, &style);
   lv_gauge_set_needle_count(gauge1, 1, needle_colors);
   lv_obj_set_size(gauge1, 180, 180);
