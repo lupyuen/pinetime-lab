f8dc3794ef895af54f3cef8c10e0bbd98d548bf9
Sravan <65145081+ZephyrLabs@users.noreply.github.com>
2020-09-08 06:55:27 +0530

    Update Clock.cpp

--- src/DisplayApp/Screens/Clock.cpp
+++ src/DisplayApp/Screens/Clock.cpp
@@ -19,37 +19,6 @@ static void event_handler(lv_obj_t * obj, lv_event_t event) {
   screen->OnObjectEvent(obj, event);
 }
 
-//analog
- 
-  /*Create a style*/
-//  lv_style_copy(&style, &lv_style_pretty_color);
-//  style.body.main_color = LV_COLOR_WHITE;     /*Line color at the beginning*/
-//  style.body.grad_color =  LV_COLOR_WHITE;    /*Line color at the end*/
-//  style.body.padding.left = 10;                      /*Scale line length*/
-//  style.body.padding.inner = 8 ;                    /*Scale label padding*/
-//  style.body.border.color = lv_color_hex3(0x333);   /*Needle middle circle color*/
-// style.line.width = 3;
-//  style.text.color = LV_COLOR_WHITE;
-// style.line.color = LV_COLOR_WHITE;                  /*Line color after the critical value*/
-
-  /*Describe the color for the needles*/
-  static lv_color_t needle_colors[1];
-  needle_colors[0] = LV_COLOR_ORANGE;
-
-  /*Create a gauge*/
-  lv_obj_t * gauge1 = lv_gauge_create(lv_scr_act(), NULL);
-//lv_gauge_set_style(gauge1, LV_GAUGE_STYLE_MAIN, &style);
-  lv_gauge_set_needle_count(gauge1, 1, needle_colors);
-  lv_obj_set_size(gauge1, 180, 180);
-  lv_obj_align(gauge1, NULL, LV_ALIGN_CENTER, 0, 0);
-  lv_gauge_set_scale(gauge1, 360, 60, 0);
-  lv_gauge_set_range(gauge1, 0, 59);
-
-  /*Set the values*/
-  // lv_gauge_set_value(gauge1, 0, minute*36);                                            
-                                             
-//analog  
-
 Clock::Clock(DisplayApp* app,
         Controllers::DateTime& dateTimeController,
         Controllers::Battery& batteryController,
@@ -108,6 +77,38 @@ Clock::Clock(DisplayApp* app,
   stepIcon = lv_label_create(lv_scr_act(), NULL);
   lv_label_set_text(stepIcon, Symbols::shoe);
   lv_obj_align(stepIcon, stepValue, LV_ALIGN_OUT_LEFT_MID, -5, 0);
+    
+  //analog
+ 
+  /*Create a style*/
+//  lv_style_copy(&style, &lv_style_pretty_color);
+//  style.body.main_color = LV_COLOR_WHITE;     /*Line color at the beginning*/
+//  style.body.grad_color =  LV_COLOR_WHITE;    /*Line color at the end*/
+//  style.body.padding.left = 10;                      /*Scale line length*/
+//  style.body.padding.inner = 8 ;                    /*Scale label padding*/
+//  style.body.border.color = lv_color_hex3(0x333);   /*Needle middle circle color*/
+// style.line.width = 3;
+//  style.text.color = LV_COLOR_WHITE;
+// style.line.color = LV_COLOR_WHITE;                  /*Line color after the critical value*/
+
+  /*Describe the color for the needles*/
+  static lv_color_t needle_colors[1];
+  needle_colors[0] = LV_COLOR_ORANGE;
+
+  /*Create a gauge*/
+  lv_obj_t * gauge1 = lv_gauge_create(lv_scr_act(), NULL);
+//lv_gauge_set_style(gauge1, LV_GAUGE_STYLE_MAIN, &style);
+  lv_gauge_set_needle_count(gauge1, 1, needle_colors);
+  lv_obj_set_size(gauge1, 180, 180);
+  lv_obj_align(gauge1, NULL, LV_ALIGN_CENTER, 0, 0);
+  lv_gauge_set_scale(gauge1, 360, 60, 0);
+  lv_gauge_set_range(gauge1, 0, 59);
+
+  /*Set the values*/
+  // lv_gauge_set_value(gauge1, 0, minute*36);                                            
+                                             
+//analog  
+                                                                                        
 }
 
 Clock::~Clock() {
